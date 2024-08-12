3f6456d2d063b7e39582a4f9383a31f9192899ee
Esteban Barragan Muñoz <esteban@akaunt.io>
2024-08-12 15:51:55 -0700

    feat: compute values based on state and render results

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ function App() {
   const [userInputValues, setUserInputValues] = useState({
     initialInvestment: 10000,
     annualInvestment: 1200,
-    expectedReturn: 1000,
+    expectedReturn: 6,
     duration: 10,
   })
   function onChangeInput(inputKey, value) {
     setUserInputValues(previousState => {
       return {
         ...previousState,
-        [inputKey]: value,
+        [inputKey]: +value,
       }
     })
   }
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,4 +1,30 @@
+import { calculateInvestmentResults, formatter } from '../util/investment';
 export default function Results({ inputValues }) {
     console.log(inputValues);
-    return <p>Results: coming soon...</p>
+    const resultData = calculateInvestmentResults(inputValues)
+    console.log(resultData);
+    return <table id='result'>
+        <thead>
+            <tr>
+                <td>Year</td>
+                <td>Investment Value</td>
+                <td>Interest (Year)</td>
+                <td>Total Interest</td>
+                <td>Invest Capital</td>
+            </tr>
+        </thead>
+        <tbody>
+            {resultData.map(data => {
+                const totalInterest = data.valueEndOfYear - data.annualInvestment * data.year - inputValues.initialInvestment;
+                const totalInvested = data.valueEndOfYear - totalInterest
+                return <tr key={data.year}>
+                    <td>{data.year}</td>
+                    <td>{formatter.format(data.valueEndOfYear)}</td>
+                    <td>{formatter.format(data.interest)}</td>
+                    <td>{formatter.format(totalInterest)}</td>
+                    <td>{formatter.format(totalInvested)}</td>
+                </tr>
+            })}
+        </tbody>
+    </table>
 }
